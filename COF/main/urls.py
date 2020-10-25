19407867ae026ba0b95a4ddfa9e49ba7cec89d42
chaselee <chaselee.13@gmail.com>
2020-10-25 17:02:30 -0600

    Added URLS / Login Skeleton
    Added URLS / Login Skeleton

--- COF/main/urls.py
+++ COF/main/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='login'),
+	path('', views.home, name='home'), 
+    path('login/', views.login, name='login'),
+	path('logout/' views.logout, name='logout'),
+	path('create_account/', views.create_account, name='create_account'),
+	path('dashboard/', views.user_dashboard, name='user_dashboard')
 ]
--- COF/main/views.py
+++ COF/main/views.py
@@ -1,5 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import User
+
+def home(request):
+	if request.user.is_authenticated:
+		return redirect('user_dashboard')
+	else:
+		return redirect('login')
 
 def login(request):
-    return HttpResponse('Hi! This is Covid on Flight speaking. ✈️')
+	if request.user.is_authenticated:
+		return redirect('user_dashboard')
+	
+	if request.method == "POST":
+		email = request.POST['email']
+		password = request.POST['password']
+		user = authenticate(email=email, password=password)
+		if user is not None:
+			django_login(request,user)
+			return redirect('user_dashboard')
+		else:
+			return HttpResponse('Invalid Username/Password')
+
+	return HttpResponse('Login_Form')
+
+def logout(request):
+	if request.user.is_authenticated:
+		django_logout(request)
+
+	return redirect('home')
+
+def create_account(request):
+	if request.user.is_authenticated:
+		return redirect('user_dashboard')
+	
+	return render(request, AuthenticationForm)
+	return redirect('user_dashboard')
+
+
+def user_dashboard(request):
+	return HttpResponse('User_Dashboard Vue ✈️')
