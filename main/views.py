8ab4a2652befc83c03c0489de13ec8654a54a07b
Stephen Smith <autrydev@gmail.com>
2020-10-31 18:02:07 -0400

    Release 1.1 (#9)
    * Update settings.py
    
    * Auth/urls (#8)
    
    * Added URLS / Login Skeleton
    
    Added URLS / Login Skeleton
    
    * 10/27 Update
    
    * 10/27 Update 2
    
    * Modify Login URLs and Disable URL Security
    
    * Move Files to Root Directory
    
    * Add .gitignore and Delete PyCache
    
    * Update settings.py
    
    * Update settings.py
    
    * Delete db.sqlite3
    
    Co-authored-by: chaselee <chaselee.13@gmail.com>
    
    Co-authored-by: chaselee <chaselee.13@gmail.com>

--- main/views.py
+++ main/views.py
@@ -1,3 +1,65 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import User
+
+def home(request):
+	#Reserved for homepage..currently redirects
+	if request.user.is_authenticated:
+		return redirect('user_dashboard')
+	else:
+		return redirect('login')
+
+def login(request):
+	if request.user.is_authenticated:
+		return redirect('user_dashboard')
+	
+	if request.method == "POST":
+	#Uses Django Authentication
+		email = request.POST['email']
+		password = request.POST['password']
+		user = authenticate(username=email, password=password)
+		if user is not None:
+			#django_login(request,user)
+			return redirect('user_dashboard')
+		else:
+			return HttpResponse('Invalid Email/Password', status=401)
+
+def logout(request):
+	if request.user.is_authenticated:
+		django_logout(request)
+
+	return redirect('home')
+
+def signup(request):
+	if request.user.is_authenticated:
+		return redirect('user_dashboard')
+	
+	if request.method == "POST":
+		email = request.POST['email']
+		password = request.POST['password']
+		first_name = request.POST['first_name']
+		last_name = request.POST['last_name']
+		phone_number = request.POST['phone_number']
+		checkuser = authenticate(username=email)
+
+		if checkuser is not None:
+			return HttpResponse('Username Already Exists')
+		else:
+			#Create COFUser & Django User
+			user_mgr = COFUserManager()
+			user_mgr.create_user(email, password, first_name, last_name, phone_number)
+			py_user = User.objects.create_user(username=email, email=email, first_name=first_name, last_name=last_name)
+			py_user.save()
+			return py_user #or COFUser..?
+
+def user_dashboard(request):
+	return HttpResponse('User_Dashboard Vue ✈️')
+
+def admin_dashboard(request):
+	return HttpResponse('Admin_Dashboard Vue ✈️')
+=======
 from django.shortcuts import render
 from django.http import HttpResponse
 
