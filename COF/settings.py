15eda0aa97baffd85a8fd9c6990e8d4eaba71124
sadiecrawford <sadiecrawford@ufl.edu>
2020-11-04 08:50:24 -0500

    Related_name fix

--- COF/settings.py
+++ COF/settings.py
@@ -124,4 +124,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+AUTH_USER_MODEL = 'main.COFUser'
+
 django_heroku.settings(locals())
--- main/models.py
+++ main/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Creates customer Manager for cofUser object because the regular User object is being extended
-class cofUserManager(BaseUserManager):
+# Creates customer Manager for COFUser object because the regular User object is being extended
+class COFUserManager(BaseUserManager):
     # Creates a regular User and saves into the database
     def create_user(self, email, password, first_name, last_name, phone_number, covid_status='Unknown', last_update=None):
         user = self.model(
@@ -39,8 +39,8 @@ class cofUserManager(BaseUserManager):
         return user
 
 
-# Defines the cofUser object model
-class cofUser(AbstractUser):
+# Defines the COFUser object model
+class COFUser(AbstractUser):
     username = None
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=50)
@@ -52,7 +52,7 @@ class cofUser(AbstractUser):
     )
     last_update = models.DateField(null=True, blank=True)
 
-    objects = cofUserManager()
+    objects = COFUserManager()
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name', 'phone_number']
 
@@ -63,7 +63,7 @@ class cofUser(AbstractUser):
 # Defines the RecoveryCombination object model
 class RecoveryCombination(models.Model):
     email = models.ForeignKey(
-        'cofUser',
+        'COFUser',
         on_delete=models.CASCADE
     )
     recovery_code = models.CharField(max_length=12) # TODO: Hash recovery codes
@@ -102,7 +102,7 @@ class Flight(models.Model):
 # Defines the FlightsTaken object model
 class FlightsTaken(models.Model):
     email = models.ForeignKey(
-        'cofUser',
+        'COFUser',
         on_delete=models.CASCADE
     )
     survey_id = models.ForeignKey(
