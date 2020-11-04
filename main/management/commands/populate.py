a3d16682f6bd77e35b5c27e0351d0941421dab60
sadiecrawford <sadiecrawford@ufl.edu>
2020-11-04 14:50:12 -0500

    Added relative flight test data generation
    - Dates generated relative to current date

--- main/management/commands/populate.py
+++ main/management/commands/populate.py
@@ -3,6 +3,7 @@ from django.core.management.base import BaseCommand, CommandError
 from main.models import *
 from django.contrib.auth import get_user_model
 from django.db.utils import IntegrityError
+import datetime
 
 class Command(BaseCommand):
     help = 'Fills database with test data'
@@ -16,14 +17,15 @@ class Command(BaseCommand):
             call_command('migrate')
 
             # Add users
+            self.stdout.write('============Users============')
             admin = get_user_model().objects.create_user(
-                            email='admin@admin.com',
-                            password='admin',
-                            first_name='admin',
-                            last_name='admin',
-                            phone_number='1231231234',
-                            covid_status='Unknown',
-                            last_update=None,
+                email='admin@admin.com',
+                password='admin',
+                first_name='admin',
+                last_name='admin',
+                phone_number='1231231234',
+                covid_status='Unknown',
+                last_update=None,
             )
             admin.is_staff = True
             admin.is_superuser = True
@@ -32,39 +34,39 @@ class Command(BaseCommand):
             self.stdout.write(self.style.SUCCESS('added!'))
 
             alligator = get_user_model().objects.create_user(
-                            email='gogators@ufl.edu',
-                            password='GatorsRule!',
-                            first_name='Alli',
-                            last_name='Gator',
-                            phone_number='3523923261',
-                            covid_status='Unknown',
-                            last_update=None,
+                email='gogators@ufl.edu',
+                password='GatorsRule!',
+                first_name='Alli',
+                last_name='Gator',
+                phone_number='3523923261',
+                covid_status='Unknown',
+                last_update=None,
             )
             alligator.save()
             print('Alli Gator',end=' ')
             self.stdout.write(self.style.SUCCESS('added!'))
 
             brady = get_user_model().objects.create_user(
-                            email='info@tb12sports.co',
-                            password='GoBucsNumber12',
-                            first_name='Tom',
-                            last_name='Brady',
-                            phone_number='9177040000',
-                            covid_status='Negative',
-                            last_update='2020-10-09',
+                email='info@tb12sports.co',
+                password='GoBucsNumber12',
+                first_name='Tom',
+                last_name='Brady',
+                phone_number='9177040000',
+                covid_status='Negative',
+                last_update='2020-10-09',
             )
             brady.save()
             print('Tom Brady',end=' ')
             self.stdout.write(self.style.SUCCESS('added!'))
 
             bastian = get_user_model().objects.create_user(
-                            email='edward.bastian@delta.com',
-                            password='nileToMedSea4',
-                            first_name='Edward',
-                            last_name='Bastian',
-                            phone_number='4047730305',
-                            covid_status='Negative',
-                            last_update='2020-10-27',
+                email='edward.bastian@delta.com',
+                password='nileToMedSea4',
+                first_name='Edward',
+                last_name='Bastian',
+                phone_number='4047730305',
+                covid_status='Negative',
+                last_update='2020-10-27',
             )
             bastian.is_staff = True
             bastian.save()
@@ -72,16 +74,77 @@ class Command(BaseCommand):
             self.stdout.write(self.style.SUCCESS('added!'))
 
             matthews = get_user_model().objects.create_user(
-                            email='iloveparties@hotmail.com',
-                            password='itraveltoparty123',
-                            first_name='Jake',
-                            last_name='Matthews',
-                            phone_number='7043628925',
-                            covid_status='Positive',
-                            last_update='2020-10-27',
+                email='iloveparties@hotmail.com',
+                password='itraveltoparty123',
+                first_name='Jake',
+                last_name='Matthews',
+                phone_number='7043628925',
+                covid_status='Positive',
+                last_update='2020-10-27',
             )
             matthews.save()
             print('Jake Matthews',end=' ')
             self.stdout.write(self.style.SUCCESS('added!'))
+
+            # Flights
+            self.stdout.write('============Flights============')
+            today_date = datetime.datetime.now().date()
+
+            one_day_past = datetime.timedelta(days=0)
+            lax_jfk = Flight.objects.create(
+                flight_id = 'DL1742',
+                departure_city = 'LAX',
+                arrival_city = 'JFK',
+                date = str(today_date - one_day_past),
+                departure_time = '15:00:00',
+                arrival_time = '18:45:00',
+                covid_count = 1
+            )
+            lax_jfk.save()
+            print('LAX -> JFK', lax_jfk.date, '15:00:00', end=' ')
+            self.stdout.write(self.style.SUCCESS('added!'))
+
+            four_days_future = datetime.timedelta(days=4)
+            avl_pie = Flight.objects.create(
+                flight_id = 'AA1380',
+                departure_city = 'AVL',
+                arrival_city = 'PIE',
+                date = str(today_date + four_days_future),
+                departure_time = '20:00:00',
+                arrival_time = '21:30:00',
+                covid_count = 0
+            )
+            avl_pie.save()
+            print('AVL -> PIE', avl_pie.date, '20:00:00', end=' ')
+            self.stdout.write(self.style.SUCCESS('added!'))
+
+            seven_days_future = datetime.timedelta(days=7)
+            pie_avl = Flight.objects.create(
+                flight_id = 'AA1029',
+                departure_city = 'PIE',
+                arrival_city = 'AVL',
+                date = str(today_date + seven_days_future),
+                departure_time = '12:00:00',
+                arrival_time = '13:30:00',
+                covid_count = 0
+            )
+            pie_avl.save()
+            print('PIE -> AVL', pie_avl.date, '12:00:00', end=' ')
+            self.stdout.write(self.style.SUCCESS('added!'))
+
+            twelve_days_past = datetime.timedelta(days=12)
+            dtw_phx = Flight.objects.create(
+                flight_id = 'NK6127',
+                departure_city = 'DTW',
+                arrival_city = 'PHX',
+                date = str(today_date - twelve_days_past),
+                departure_time = '17:00:00',
+                arrival_time = '20:00:00',
+                covid_count = 2
+            )
+            dtw_phx.save()
+            print('DTW -> PHX', dtw_phx.date, '17:00:00', end=' ')
+            self.stdout.write(self.style.SUCCESS('added!'))
+
         except IntegrityError:
             raise CommandError('Uniqueness IntegrityError. Please delete db.sqlite3 before running this command.')
