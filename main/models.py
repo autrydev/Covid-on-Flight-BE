98bdd04d9975aca11902060e72a970e1829517ca
Stephen Smith <autrydev@gmail.com>
2020-11-12 21:30:34 -0400

    Update models.py

--- main/models.py
+++ main/models.py
@@ -72,7 +72,7 @@ class RecoveryCombination(models.Model):
 # Defines the Survey object model
 class Survey(models.Model):
     # IDs already automatically created by Django
-    results = models.CharField(max_length=8, blank=True)
+    results = models.CharField(max_length=20, blank=True)
     covid_test = models.BooleanField() # Yes, No
     test_results = models.CharField(max_length=3, default='N/A') # Yes, No, N/A
     # The following are Yes/No's:
