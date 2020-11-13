776a87b110c469559c6afd3d6e77b82182f6ad5e
Stephen Smith <autrydev@gmail.com>
2020-11-12 21:09:31 -0400

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-python manage.py migrate admin zero
-python manage.py migrate auth zero
-python manage.py migrate contenttypes zero
-python manage.py migrate sessions zero
-python manage.py makemigrations main
-python manage.py migrate main
-python manage.py makemigrations
-python manage.py migrate
 python manage.py populate
