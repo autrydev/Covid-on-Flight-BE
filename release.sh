2d5049d2621effaeea93230afab903a8068403e6
Stephen Smith <autrydev@gmail.com>
2020-11-12 21:34:08 -0400

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+ls
+echo ("------------")
+cd ./main
+ls
+echo ("------------")
+rm -r migrations
+cd ..
+echo ("------------")
+cd ./cof
+ls
+echo ("------------")
+rm db.sqlite3
+ls
+echo ("------------")
 python manage.py testdata
